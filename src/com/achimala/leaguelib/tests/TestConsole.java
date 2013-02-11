1fc8e7cfdc516bf9190904a25452eaed08d0adfa
Anshu Chimala <achimala@gmail.com>
2013-02-11 14:56:12 -0800

    Finished console

--- src/com/achimala/leaguelib/tests/TestConsole.java
+++ src/com/achimala/leaguelib/tests/TestConsole.java
@@ -4,19 +4,46 @@ package com.achimala.leaguelib.tests;
 // import com.achimala.leaguelib.models.*;
 import com.gvaneyck.rtmp.*;
 import java.io.*;
+import java.util.*;
+import com.google.gson.*;
 
 public class TestConsole {
     public static void main(String[] args) throws Exception {
-        Console console = System.console();
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        
         String username, password, client, server;
-        username = console.readLine("Username: ");
-        password = new String(console.readPassword("Password: "));
-        server = console.readLine("  Server: ");
-        client = console.readLine("  Client: ");
+        username = args[0];
+        password = args[1];
+        server = args[2];
+        client = args[3];
+        
+        System.out.println("Logging into " + server + " as " + username);
         LoLRTMPSClient lol = new LoLRTMPSClient(server, client, username, password);
         lol.connectAndLogin();
+        
+        Gson gson = new Gson();
+        
         for(;;) {
-            String cmd[] = console.readLine("> ").split(" ");
+            System.out.print("> ");
+            String cmd[] = in.readLine().split(" ");
+            if(cmd[0].equals("exit") || cmd[0].equals("quit") || cmd[0].equals("q"))
+                break;
+            if(cmd.length < 2) {
+                System.out.println("USAGE: <service> <function> <args...>");
+                continue;
+            }
+            
+            String service = cmd[0];
+            String function = cmd[1];
+            String arguments = "";
+            for(int i = 2; i < cmd.length; i++)
+                arguments += cmd[i] + " ";
+            Object[] params = gson.fromJson(arguments, ArrayList.class).toArray(new Object[0]);
+            System.out.println(String.format("CALL %s.%s(%s)", service, function, params.toString()));
+            
+            int id = lol.invoke(service, function, params);
+            System.out.println("RECV " + lol.getResult(id));
+            System.out.println();
         }
     }
 }
---
+++ test-console.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+ant jar
+java -cp build/jar/leaguelib.jar com.achimala.leaguelib.tests.TestConsole anshuchimala2 dogmeat1 NA 3.01.xxx
