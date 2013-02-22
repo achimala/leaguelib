ba36c602d5e0693c7de2d351dae94fb0531509a9
Anshu Chimala <achimala@gmail.com>
2013-02-22 00:27:02 -0800

    Added match history models/API calls; possibly incomplete but functional

---
+++ src/com/achimala/leaguelib/models/MatchHistoryStatType.java
@@ -0,0 +1,63 @@
+/*
+ *  This file is part of LeagueLib.
+ *  LeagueLib is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  LeagueLib is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with LeagueLib.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.achimala.leaguelib.models;
+
+public enum MatchHistoryStatType {
+    ASSISTS,
+    BARRACKS_KILLED,
+    CHAMPIONS_KILLED,
+    COMBAT_PLAYER_SCORE,
+    GOLD_EARNED,
+    ITEM0,
+    ITEM1,
+    ITEM2,
+    ITEM3,
+    ITEM4,
+    ITEM5,
+    LARGEST_CRITICAL_STRIKE,
+    LARGEST_KILLING_SPREE,
+    LARGEST_MULTI_KILL,
+    LEVEL,
+    LOSE,
+    MAGIC_DAMAGE_DEALT_PLAYER,
+    MAGIC_DAMAGE_DEALT_TO_CHAMPIONS,
+    MAGIC_DAMAGE_TAKEN,
+    MINIONS_KILLED,
+    NEUTRAL_MINIONS_KILLED,
+    NODE_CAPTURE,
+    NODE_CAPTURE_ASSIST,
+    NODE_NEUTRALIZE,
+    NODE_NEUTRALIZE_ASSIST,
+    NUM_DEATHS,
+    OBJECTIVE_PLAYER_SCORE,
+    PHYSICAL_DAMAGE_DEALT_PLAYER,
+    PHYSICAL_DAMAGE_DEALT_TO_CHAMPIONS,
+    PHYSICAL_DAMAGE_TAKEN,
+    SIGHT_WARDS_BOUGHT_IN_GAME,
+    TEAM_OBJECTIVE,
+    TOTAL_DAMAGE_DEALT,
+    TOTAL_DAMAGE_DEALT_TO_CHAMPIONS,
+    TOTAL_DAMAGE_TAKEN,
+    TOTAL_HEAL,
+    TOTAL_PLAYER_SCORE,
+    TOTAL_SCORE_RANK,
+    TOTAL_TIME_SPENT_DEAD,
+    TURRETS_KILLED,
+    VICTORY_POINT_TOTAL,
+    VISION_WARDS_BOUGHT_IN_GAME,
+    WIN,
+}
