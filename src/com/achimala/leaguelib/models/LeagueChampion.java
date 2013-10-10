4b69d7bc466a164546b64e3ce7da94b9e0e901b3
Anshu Chimala <achimala@gmail.com>
2013-10-10 00:54:05 -0700

    Added Jinx

--- src/com/achimala/leaguelib/models/LeagueChampion.java
+++ src/com/achimala/leaguelib/models/LeagueChampion.java
@@ -142,6 +142,7 @@ public class LeagueChampion {
         _modelMap.put(127, "Lissandra");
         _modelMap.put(266, "Aatrox");
         _modelMap.put(236, "Lucian");
+        _modelMap.put(222, "Jinx");
         
         _champMap = new HashMap<Integer, LeagueChampion>();
     }
